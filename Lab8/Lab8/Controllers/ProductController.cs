97a389585b83a73f1b88765234ebde313155e0d7
Max1202 <melnikovmaks1202@gmail.com>
2023-10-24 16:58:16 +0300

    add Lab8

---
+++ Lab8/Lab8/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Lab8.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab8.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Product()
+        {
+            var products = new List<Product>
+        {
+            new Product { Id = 1, Name = "iPhone 15 Pro Max", Price = 2187.89M, CreatedDate = DateTime.Now },
+            new Product { Id = 2, Name = "Asus TUF Gaming F15", Price = 700M, CreatedDate = DateTime.Now },
+            new Product { Id = 3, Name = "Samsung Galaxy Fold 5", Price = 2051.15M, CreatedDate = DateTime.Now },
+            new Product { Id = 4, Name = "Телевізор \" Xiaomi Mi TV Q1 75\"", Price = 1367.43M, CreatedDate = DateTime.Now }
+        };
+
+            return View(products);
+        }
+    }
+}
