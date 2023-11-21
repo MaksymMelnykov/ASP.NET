f6c015d70dda4d47a64cba37c8dc4e47cec62128
Maksym <92988201+MaksymMelnykov@users.noreply.github.com>
2023-11-21 17:14:53 +0200

    Update Lab12

--- Lab12/Lab12/Models/Company.cs
+++ Lab12/Lab12/Models/Company.cs
@@ -5,7 +5,6 @@ namespace Lab12.Models
     public class Company
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "Company Name is required.")]
         public string CompanyName { get; set; }
         public int NumberOfEmployees { get; set;}
         public int FoundationYear { get; set; }
